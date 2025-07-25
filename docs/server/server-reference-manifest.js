aaf04a6890c7f129594c7d6a13564b26e72cb7b8
bachnvse <Bachnvhe172297@fpt.edu.vn>
2025-07-25 14:32:37 +0700

    fix

--- docs/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Iy8AiQxkW54GyuUa3Pja5ToxNmhaDyGGc0Nr9HS7dmU=\"\n}"
--- docs/static/chunks/src_app_page_tsx_6ac9fe63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_95a9ea56._.js",
-  "static/chunks/src_components_fb4c48c6._.js",
-  "static/chunks/node_modules_swiper_62da158d._.css"
-],
-    source: "dynamic"
-});
