82fedf37cec90a9997555473951a0bb93a4e23e9
bachnvse <Bachnvhe172297@fpt.edu.vn>
2025-07-25 12:32:58 +0700

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'docs',
   images: {
+    unoptimized: true, // Bắt buộc khi export static
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'fakestoreapi.com',
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
