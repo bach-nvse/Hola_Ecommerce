835fcbd91c409f3082872b61ef0571fd435ab569
bachnvse <Bachnvhe172297@fpt.edu.vn>
2025-07-25 12:20:37 +0700

    pu

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
+  assetPrefix: '/', // dùng root domain nên để '/'
   typescript: {
-    ignoreBuildErrors: true, // Bỏ qua type lỗi khi export static
+    ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true, // tắt Image Optimization cho static export
     remotePatterns: [
       {
         protocol: 'https',
